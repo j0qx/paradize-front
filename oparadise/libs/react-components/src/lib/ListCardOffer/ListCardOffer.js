cb708734d04a22b8b98c8fc2212f36b088af89fb
j0qx <vasseurb.pro@gmail.com>
2021-10-27 16:26:57 +0200

    add ListCardOffer

---
+++ oparadise/libs/react-components/src/lib/ListCardOffer/ListCardOffer.js
@@ -0,0 +1,9 @@
+import './ListCardOffer.module.scss';
+export function ListCardOffer(props) {
+  return (
+    <div>
+      <h1>Welcome to ListCardOffer!</h1>
+    </div>
+  );
+}
+export default ListCardOffer;
---
+++ oparadise/libs/react-components/src/lib/ListCardOffer/ListCardOffer.stories.js
@@ -0,0 +1,11 @@
+import ListCardOffer from './ListCardOffer';
+
+export default {
+  component: ListCardOffer,
+  title: 'ListCardOffer',
+};
+
+const Template = (args) => <ListCardOffer {...args} />;
+
+export const Primary = Template.bind({});
+Primary.args = {};

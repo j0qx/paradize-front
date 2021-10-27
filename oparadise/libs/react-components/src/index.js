03ea8b4603f596d9754c31d78c1ca22ab04a2955
doudou3180 <ferrandpierremarie@gmail.com>
2021-10-27 16:55:08 +0200

    Fix Export index.js

--- oparadise/libs/react-components/src/index.js
+++ oparadise/libs/react-components/src/index.js
@@ -1,27 +1,16 @@
-import CardOffer from './lib/CardOffer/CardOffer';
-
+export * from './lib/CardOffer/CardOffer';
 export * from './lib/ListCardFavoriteSearch/ListCardFavoriteSearch';
-
 export * from './lib/NavBar/NavBar';
-
 export * from './lib/ButtonClose/ButtonClose';
-
 export * from './lib/Modal/Modal';
 export * from './lib/ToolTip/ToolTip';
-
 export * from './lib/CardFavoriteSearch/CardFavoriteSearch';
-
 export * from './lib/CreateOffer/CreateOffer';
 export * from './lib/Offer/Offer';
-
 export * from './lib/SearchZoneSettings/SearchZoneSettings';
 export * from './lib/ButtonSubmit/ButtonSubmit';
-
 export * from './lib/SearchDataSettings/SearchDataSettings';
 export * from './lib/ListCardFavoriteSearch/ListCardFavoriteSearch';
 export * from './lib/Map/Map';
-
 export * from './lib/MapSettings/MapSettings';
-
 export * from './lib/TabNavBar/TabNavBar';
-export { CardOffer };

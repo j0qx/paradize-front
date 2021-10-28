009662b2c560c827c95837ea22c2a0f73863be69
doudou3180 <ferrandpierremarie@gmail.com>
2021-10-28 21:58:30 +0200

    add 404 preparation component

--- oparadise/apps/front-react/src/components/URLError/URLError.js
+++ oparadise/apps/front-react/src/components/URLError/URLError.js
@@ -1,8 +1,27 @@
-import './URLError.module.scss';
+
+
+
+import style from './URLError.module.scss';
+import treePicture from '../../assets/image/ile.png'
+import cloud from '../../assets/image/entete.png'
+import sea from '../../assets/image/decor.png'
+
 export function URLError(props) {
+
+
+
+
   return (
-    <div>
-      <h1>Welcome to URLError!</h1>
+    <div className={style.urlError}>
+      <div className={style.urlError__inner}>
+        <div className={style.urlError__boxFront}>
+          <img className={style.urlError__cloudpic} src={cloud} alt="" />
+        </div>
+        <div className={style.urlError__boxBack}>
+          <img className={style.urlError__tree} src={treePicture} alt="" />
+          <h2 className={style.urlError__title}> 404 !</h2>
+        </div>
+      </div>
     </div>
   );
 }

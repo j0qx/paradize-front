05afc837e1ae47f008d943f775cb483dd9bb4680
j0qx <vasseurb.pro@gmail.com>
2021-10-28 16:19:48 +0200

    pause merge

--- oparadise/apps/front-react/src/components/Contact/Contact.js
+++ oparadise/apps/front-react/src/components/Contact/Contact.js
@@ -10,12 +10,19 @@ const Contact = () => {
         <h1 className={style.container__contact__card__role}>Rôle</h1>
         <h2>Lead Développeur front-end</h2>
       </div>
-      <div className={style.contact__card}>
+      <div className={style.container__contact__card}>
         <img className={style.container__contact__card__img}
       src="https://avatars.githubusercontent.com/u/64959890?v=4.png"
       alt="Avatar" />
         <h1 className={style.container__contact__card__role}>Rôle</h1>
-        <h2>Scrum-Master</h2>
+        <h2>Lead Développeur front-end</h2>
+      </div>
+      <div className={style.container__contact__card}>
+        <img className={style.container__contact__card__img}
+      src="https://avatars.githubusercontent.com/u/64959890?v=4.png"
+      alt="Avatar" />
+        <h1 className={style.container__contact__card__role}>Rôle</h1>
+        <h2>Lead Développeur front-end</h2>
       </div>
     </div>
     

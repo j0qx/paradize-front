fe4dafb9c79e579a4113b6f50b13bf8e8fae3cda
doudou3180 <ferrandpierremarie@gmail.com>
2021-10-28 16:03:32 +0200

    add burger position

--- oparadise/apps/front-react/src/components/Header/Header.js
+++ oparadise/apps/front-react/src/components/Header/Header.js
@@ -1,6 +1,8 @@
 // import component
 import { ButtonSubmit } from '@oparadise/react-components';
 import logo from '../../assets/image/logo.svg';
+
+//import { faBars } from '@fortawesome/free-solid-svg-icons'
 import style from './Header.module.scss';
 
 export function Header(props) {
@@ -9,10 +11,15 @@ export function Header(props) {
     <div className={style.header}>
       <div className={style.header__mainlogo}>
         <img className={style.header__mainlogo__logo} src={logo} alt="logo de O'Paradise" />
-        <div className={style.header__burger}> Burger </div>
+        <i className={` ${style.header__burger} fas fa-bars fa-4x`}  />
+
       </div>
       <span>Welcome Prénom</span>
-      <ButtonSubmit className={style.header__button} />
+      <ButtonSubmit
+      className={style.header__button}
+      //classCSS
+      //buttonName
+      />
       <ButtonSubmit className={style.header__button} />
     </div>
   );

8ea3761e3d4e228fd38a91059c26b449a455a236
doudou3180 <ferrandpierremarie@gmail.com>
2021-10-28 16:22:58 +0200

    modify different settings on NavBar

--- oparadise/libs/react-components/src/lib/NavBar/NavBar.js
+++ oparadise/libs/react-components/src/lib/NavBar/NavBar.js
@@ -4,6 +4,7 @@ export function NavBar(props) {
   return (
 
 // Both h2 are becoming NavLink after Router is going to be OK.
+// Need to link  both pages
     <div className={style.navBar_Container}>
       <h2 className={style.navBar_Container_Title}>Mentions légales</h2>
       <h2 className={style.navBar_Container_Title}>Contacts</h2>

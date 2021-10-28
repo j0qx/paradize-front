54bdedb7cee3937a8acf17b6fab136ee0938840e
doudou3180 <ferrandpierremarie@gmail.com>
2021-10-28 13:28:40 +0200

    add position Footer

--- oparadise/apps/front-react/src/components/Footer/Footer.js
+++ oparadise/apps/front-react/src/components/Footer/Footer.js
@@ -1,11 +1,11 @@
 //import component
 import { NavBar } from '@oparadise/react-components';
-import './Footer.module.scss';
+import style from './Footer.module.scss';
 export function Footer(props) {
   return (
-    <div>
-      <NavBar />
-      <h1>Welcome to Footer!</h1>
+    <div className={style.container}>
+      <h2>copyright2021©Oparadise</h2>
+      <NavBar className={style.navbar} />
     </div>
   );
 }

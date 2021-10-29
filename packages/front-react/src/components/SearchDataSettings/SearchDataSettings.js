208629d3bcb2d52a2d8b5b90af853f43cc7efaf4
j0qx <vasseurb.pro@gmail.com>
2021-10-29 18:12:32 +0200

    search data settings update

--- packages/front-react/src/components/SearchDataSettings/SearchDataSettings.js
+++ packages/front-react/src/components/SearchDataSettings/SearchDataSettings.js
@@ -1,65 +1,145 @@
+/* eslint-disable jsx-a11y/label-has-associated-control */
+import { useDispatch, useSelector } from 'react-redux';
 import style from './SearchDataSettings.module.scss';
+import { CHECKBOX_TOGGLE } from '../../store/actions';
 
-const SearchDataSettings = () => (
-  <div className={style.container__control}>
+const SearchDataSettings = () => {
+  const dispatch = useDispatch();
+  const bars = useSelector((state) => state.search.bars);
+  const medic = useSelector((state) => state.search.medic);
+  const schools = useSelector((state) => state.search.schools);
+  const shops = useSelector((state) => state.search.shops);
+  const parcs = useSelector((state) => state.search.parcs);
+  const police = useSelector((state) => state.search.police);
 
-    <label className={`${style.control__label} ${style.control__checkbox}`}> 
-            <input type="checkbox" checked="checked" />
-            Future icone 
-    </label>
+  return (
+    // Container where the whole component is sitting.
+    <div className={style.container__control}>
+      {/* Each label is meant to be a checkbox */}
+      <div className={style.container__switch}>
+        <label
+          className={style.switch}
+        >
+          {/* Each input read and change the state from the search reducer to check or uncheck   */}
+          <input
+            onChange={(e) => {
+              dispatch({
+                type: CHECKBOX_TOGGLE,
+                checkBoxField: e.target.name,
+              });
+            }}
+            name="bars"
+            type="checkbox"
+            checked={bars}
+          />
+          <span className={`${style.slider} ${style.round}`} />
+          <p className={style.slider__icon}>Future icone</p>
+        </label>
+      </div>
+      {/* Each label is meant to be a checkbox */}
+      <div className={style.container__switch}>
+        <label
+          className={style.switch}
+        >
+          <input
+            onChange={(e) => {
+              dispatch({
+                type: CHECKBOX_TOGGLE,
+                checkBoxField: e.target.name,
+              });
+            }}
+            name="medic"
+            type="checkbox"
+            checked={medic}
+          />
+          <span className={`${style.slider} ${style.round}`} />
+          <p className={style.slider__icon}>Future icone</p>
+        </label>
+      </div>
+      {/* Each label is meant to be a checkbox */}
+      <div className={style.container__switch}>
 
-    <label className={`${style.control__label} ${style.control__checkbox}`}> 
-            <input type="checkbox" checked="checked" />
-            Future icone 
-    </label>
+        <label
+          className={style.switch}
+        >
+          <input
+            onChange={(e) => {
+              dispatch({
+                type: CHECKBOX_TOGGLE,
+                checkBoxField: e.target.name,
+              });
+            }}
+            name="shops"
+            type="checkbox"
+            checked={shops}
+          />
+          <span className={`${style.slider} ${style.round}`} />
+          <p className={style.slider__icon}>Future icone</p>
+        </label>
+      </div>
+      {/* Each label is meant to be a checkbox */}
+      <div className={style.container__switch}>
 
-    <label className={`${style.control__label} ${style.control__checkbox}`}> 
-            <input type="checkbox" checked="checked" />
-            Future icone 
-    </label>
+        <label
+          className={style.switch}
+        >
+          <input
+            onChange={(e) => {
+              dispatch({
+                type: CHECKBOX_TOGGLE,
+                checkBoxField: e.target.name,
+              });
+            }}
+            name="parcs"
+            type="checkbox"
+            checked={parcs}
+          />
+          <span className={`${style.slider} ${style.round}`} />
+          <p className={style.slider__icon}>Future icone</p>
+        </label>
+      </div>
+      {/* Each label is meant to be a checkbox */}
+      <div className={style.container__switch}>
 
-    <label className={`${style.control__label} ${style.control__checkbox}`}> 
-            <input type="checkbox" checked="checked" />
-            Future icone 
-    </label>
-
-    <label className={`${style.control__label} ${style.control__checkbox}`}> 
-            <input type="checkbox" checked="checked" />
-            Future icone 
-    </label>
-
-    <label className={`${style.control__label} ${style.control__checkbox}`}> 
-            <input type="checkbox" checked="checked" />
-            Future icone 
-    </label>
-
-    <label className={`${style.control__label} ${style.control__checkbox}`}> 
-            <input type="checkbox" checked="checked" />
-            Future icone 
-    </label>
-
-    <label className={`${style.control__label} ${style.control__checkbox}`}> 
-            <input type="checkbox" checked="checked" />
-            Future icone 
-    </label>
-
-    <label className={`${style.control__label} ${style.control__checkbox}`}> 
-            <input type="checkbox" checked="checked" />
-            Future icone 
-    </label>
-    <label className={`${style.control__label} ${style.control__checkbox}`}> 
-            <input type="checkbox" checked="checked" />
-            Future icone 
-    </label>
-    <label className={`${style.control__label} ${style.control__checkbox}`}> 
-            <input type="checkbox" checked="checked" />
-            Future icone 
-    </label>
-    <label className={`${style.control__label} ${style.control__checkbox}`}> 
-            <input type="checkbox" checked="checked" />
-            Future icone 
-    </label>
+        <label
+          className={style.switch}
+        >
+          <input
+            onChange={(e) => {
+              dispatch({
+                type: CHECKBOX_TOGGLE,
+                checkBoxField: e.target.name,
+              });
+            }}
+            name="police"
+            type="checkbox"
+            checked={police}
+          />
+          <span className={`${style.slider} ${style.round}`} />
+          <p className={style.slider__icon}>Future icone</p>
+        </label>
+      </div>
+      {/* Each label is meant to be a checkbox */}
+      <label
+        className={style.switch}
+      >
+        <input
+          onChange={(e) => {
+            dispatch({
+              type: CHECKBOX_TOGGLE,
+              checkBoxField: e.target.name,
+            });
+          }}
+          name="schools"
+          type="checkbox"
+          checked={schools}
+        />
+        <span className={`${style.slider} ${style.round}`} />
+      </label>
+      <p className={style.slider__icon}>Future icone</p>
+      {/* Each label is meant to be a checkbox */}
     </div>
-);
+  );
+};
 
 export default SearchDataSettings;
--- packages/front-react/src/store/reducers/index.js
+++ packages/front-react/src/store/reducers/index.js
@@ -1,8 +1,12 @@
 import { combineReducers } from 'redux';
 import userReducer from './user';
+import searchReducer from './search';
+import dataApiReducer from './dataApi';
 
 const rootReducer = combineReducers({
   user: userReducer,
+  search: searchReducer,
+  dataApi: dataApiReducer,
 });
 
 export default rootReducer;

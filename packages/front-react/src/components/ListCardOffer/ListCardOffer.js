895b0a0dfee6decea88a131331148ff0ccd4b636
doudou3180 <ferrandpierremarie@gmail.com>
2021-10-29 17:01:36 +0200

    create cardOffer page and listCardOffer slide

--- packages/front-react/src/components/ListCardOffer/ListCardOffer.js
+++ packages/front-react/src/components/ListCardOffer/ListCardOffer.js
@@ -1,9 +1,71 @@
-import './ListCardOffer.module.scss';
+import style from './ListCardOffer.module.scss';
 import CardOffer from '../CardOffer';
 
-const ListCardOffer = () => (
-  <div>
-    <CardOffer />
-  </div>
-);
+// import files
+import house from '../../assets/image/house.jpg';
+
+const ListCardOffer = () => {
+  const tata = 'toto';
+
+// need mapping on data to dysplay real offers.
+
+  return (
+
+    <div className={style.list__container}>
+      <div className={style.list__container__title}>
+        <h3>Liste des annonces</h3>
+      </div>
+      <div className={style.list__container__main}>
+        <div className={style.list__container__main__offer}>
+          <img className={style.list__container__main__offer__picture} src={house} alt="annonce" />
+          <div className={style.list__container__main__offer__title}>
+            <p>Nom de l'annonce</p>
+          </div>
+        </div>
+        <div className={style.list__container__main__offer}>
+          <img className={style.list__container__main__offer__picture} src={house} alt="annonce" />
+          <div className={style.list__container__main__offer__title}>
+            <p>Nom de l'annonce</p>
+          </div>
+        </div>
+        <div className={style.list__container__main__offer}>
+          <img className={style.list__container__main__offer__picture} src={house} alt="annonce" />
+          <div className={style.list__container__main__offer__title}>
+            <p>Nom de l'annonce</p>
+          </div>
+        </div>
+        <div className={style.list__container__main__offer}>
+          <img className={style.list__container__main__offer__picture} src={house} alt="annonce" />
+          <div className={style.list__container__main__offer__title}>
+            <p>Nom de l'annonce</p>
+          </div>
+        </div>
+        <div className={style.list__container__main__offer}>
+          <img className={style.list__container__main__offer__picture} src={house} alt="annonce" />
+          <div className={style.list__container__main__offer__title}>
+            <p>Nom de l'annonce</p>
+          </div>
+        </div>
+        <div className={style.list__container__main__offer}>
+          <img className={style.list__container__main__offer__picture} src={house} alt="annonce" />
+          <div className={style.list__container__main__offer__title}>
+            <p>Nom de l'annonce</p>
+          </div>
+        </div>
+        <div className={style.list__container__main__offer}>
+          <img className={style.list__container__main__offer__picture} src={house} alt="annonce" />
+          <div className={style.list__container__main__offer__title}>
+            <p>Nom de l'annonce</p>
+          </div>
+        </div>
+        <div className={style.list__container__main__offer}>
+          <img className={style.list__container__main__offer__picture} src={house} alt="annonce" />
+          <div className={style.list__container__main__offer__title}>
+            <p>Nom de l'annonce</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 export default ListCardOffer;

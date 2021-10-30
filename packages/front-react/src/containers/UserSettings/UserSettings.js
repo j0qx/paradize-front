08f00eefcffd8b3edd1898e9568bc55d76ba2114
j0qx <vasseurb.pro@gmail.com>
2021-10-30 11:47:09 +0200

    update

--- packages/front-react/src/containers/UserSettings/UserSettings.js
+++ packages/front-react/src/containers/UserSettings/UserSettings.js
@@ -1,10 +1,8 @@
 import './UserSettings.module.scss';
-import SearchDataSettings from '../../components/SearchDataSettings/SearchDataSettings';
 
 const UserSettings = () => (
   <div>
     <h1>Welcome to UserSettings!</h1>
-    <SearchDataSettings />
   </div>
 );
 export default UserSettings;

e44314d79ab93137588ce7205654637fd04e80cc
Erwin-Bottius <erwinbottius@gmail.com>
2021-10-31 16:07:08 +0100

    add burger menu animation

--- packages/front-react/src/containers/Header/Header.js
+++ packages/front-react/src/containers/Header/Header.js
@@ -1,7 +1,7 @@
 // import component
-import { faBars } from '@fortawesome/free-solid-svg-icons';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { useDispatch, useSelector } from 'react-redux';
+import { Turn as Hamburger } from 'hamburger-react';
+import { useState } from 'react';
 import { ButtonSubmit } from '../../components';
 import logo from '../../assets/image/logo.svg';
 import { SET_MODAL_CONTENT, TOGGLE_PRINT_MODAL } from '../../store/actions';
@@ -10,42 +10,59 @@ import style from './Header.module.scss';
 const Header = () => {
   const dispatch = useDispatch();
   const isModalHidden = useSelector((state) => state.domSettings.isModalHidden);
+  const [isOpen, setOpen] = useState(false);
+
   return (
-    <div className={style.header}>
-      <div className={style.header__mainlogo}>
-        <img className={style.header__mainlogo__logo} src={logo} alt="logo de O'Paradise" />
-        <div className={style.header__burger}>
-          <FontAwesomeIcon icon={faBars} size="2x" />
-        </div>
+    <nav className={isOpen ? `${style.navbar} ${style.navbarOpened}` : style.navbar}>
+      <div className={style.navbar__logoContainer}>
+        <img alt={logo} src={logo} className={style.navbar__logoContainer__logo} />
       </div>
-      <span className={style.header__userName}>Welcome Prénom</span>
-      <div className={style.header__mainButtons}>
-        <ButtonSubmit
-          classCSS={style.header__mainButtons__button}
-          buttonName="Inscription"
-          handleButtonClick={() => {
-            // If the modal is already we didnt do anything
-            if (isModalHidden === false) {
-              return;
-            }
-            dispatch({ type: TOGGLE_PRINT_MODAL });
-            dispatch({ type: SET_MODAL_CONTENT, modalContent: 'isSubscribeModal' });
-          }}
-        />
-        <ButtonSubmit
-          classCSS={style.header__mainButtons__button}
-          handleButtonClick={() => {
-            // If the modal is already we didnt do anything
-            if (isModalHidden === false) {
-              return;
-            }
-            dispatch({ type: TOGGLE_PRINT_MODAL });
-            dispatch({ type: SET_MODAL_CONTENT, modalContent: 'isLoginModal' });
-          }}
-          buttonName="Connexion"
+      <p className={style.navbar__message}> Welcome Mister </p>
+      <ul className={style.navbar__linksContainer}>
+        <li className={style.navbar__linksContainer__link}>
+          <ButtonSubmit
+            classCSS={style.navbar__button}
+            buttonName="Inscription"
+            handleButtonClick={() => {
+              // If the modal is already we didnt do anything
+              if (isModalHidden === false) {
+                return;
+              }
+              // we close de nav Menu
+              setOpen(false);
+              // we print the modal
+              dispatch({ type: TOGGLE_PRINT_MODAL });
+              // we set wich modal content do we want
+              dispatch({ type: SET_MODAL_CONTENT, modalContent: 'isSubscribeModal' });
+            }}
+          />
+        </li>
+        <li className={style.navbar__linksContainer__link}>
+          <ButtonSubmit
+            classCSS={style.navbar__button}
+            handleButtonClick={() => {
+              // If the modal is already we didnt do anything
+              if (isModalHidden === false) {
+                return;
+              }
+              // we close de nav Menu
+              setOpen(false);
+              // we print the modal
+              dispatch({ type: TOGGLE_PRINT_MODAL });
+              // we set wich modal content do we want
+              dispatch({ type: SET_MODAL_CONTENT, modalContent: 'isLoginModal' });
+            }}
+            buttonName="Connexion"
+          />
+        </li>
+      </ul>
+      <div className={style.navbar__burger}>
+        <Hamburger
+          toggled={isOpen}
+          toggle={setOpen}
         />
       </div>
-    </div>
+    </nav>
   );
 };
 export default Header;

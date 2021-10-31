b25dc9b8c2bc0da6e42525c2cbe6846743c3c77a
Erwin-Bottius <erwinbottius@gmail.com>
2021-10-31 21:11:12 +0100

    add vars colors

--- packages/front-react/src/containers/Header/Header.js
+++ packages/front-react/src/containers/Header/Header.js
@@ -2,6 +2,7 @@
 import { useDispatch, useSelector } from 'react-redux';
 import { Turn as Hamburger } from 'hamburger-react';
 import { useState } from 'react';
+import { Link } from 'react-router-dom';
 import { ButtonSubmit } from '../../components';
 import logo from '../../assets/image/logo.svg';
 import { SET_MODAL_CONTENT, TOGGLE_PRINT_MODAL } from '../../store/actions';
@@ -10,19 +11,26 @@ import style from './Header.module.scss';
 const Header = () => {
   const dispatch = useDispatch();
   const isModalHidden = useSelector((state) => state.domSettings.isModalHidden);
+  const isLogged = useSelector((state) => state.user.isLogged);
+  // state about NavBar opening ( here we use usestate because the npm modul
+  // that we using for is using this way and not redux state)
   const [isOpen, setOpen] = useState(false);
 
   return (
     <nav className={isOpen ? `${style.navbar} ${style.navbarOpened}` : style.navbar}>
       <div className={style.navbar__logoContainer}>
-        <img alt={logo} src={logo} className={style.navbar__logoContainer__logo} />
+        <Link to="/">
+          <img alt={logo} src={logo} className={style.navbar__logoContainer__logo} />
+        </Link>
       </div>
       <p className={style.navbar__message}> Welcome Mister </p>
+      {/* IF User isn't connected, we show signin and sugnUp Button  */}
+      {isLogged === false && (
       <ul className={style.navbar__linksContainer}>
         <li className={style.navbar__linksContainer__link}>
           <ButtonSubmit
             classCSS={style.navbar__button}
-            buttonName="Inscription"
+            buttonName="S'inscrire"
             handleButtonClick={() => {
               // If the modal is already we didnt do anything
               if (isModalHidden === false) {
@@ -52,10 +60,34 @@ const Header = () => {
               // we set wich modal content do we want
               dispatch({ type: SET_MODAL_CONTENT, modalContent: 'isLoginModal' });
             }}
-            buttonName="Connexion"
+            buttonName="Se connecter"
           />
         </li>
       </ul>
+      )}
+      {/* IF User is connected, we show profil and logout  Button  */}
+      {isLogged && (
+      <ul className={style.navbar__linksContainer}>
+        <li className={style.navbar__linksContainer__link}>
+          <ButtonSubmit
+            classCSS={style.navbar__button}
+            buttonName="Se déconnecter"
+            handleButtonClick={() => {
+              // TODO
+            }}
+          />
+        </li>
+        <li className={style.navbar__linksContainer__link}>
+          <ButtonSubmit
+            classCSS={style.navbar__button}
+            handleButtonClick={() => {
+              // TODO
+            }}
+            buttonName="Mon compte"
+          />
+        </li>
+      </ul>
+      )}
       <div className={style.navbar__burger}>
         <Hamburger
           toggled={isOpen}
--- packages/front-react/src/store/reducers/user.js
+++ packages/front-react/src/store/reducers/user.js
@@ -1,5 +1,5 @@
 const initialState = {
-
+  isLogged: false,
 };
 
 const userReducer = (state = initialState, action = {}) => {

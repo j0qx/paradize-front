7e1c57ef4e65d65ed40bfd6c10f6ccb17d78552d
Erwin-Bottius <erwinbottius@gmail.com>
2021-11-04 15:02:35 +0100

    add useLocation Search

--- packages/front-react/src/containers/Search/Search.js
+++ packages/front-react/src/containers/Search/Search.js
@@ -1,12 +1,16 @@
+import { useLocation } from 'react-router-dom';
 import style from './Search.module.scss';
 import SearchDataSettings from '../../components/SearchDataSettings/SearchDataSettings';
 import SearchZoneSettings from '../../components/SearchZoneSettings/SearchZoneSettings';
 
-const Search = () => (
-  <div className={style.container__search}>
-    <SearchDataSettings />
-    <SearchZoneSettings />
-  </div>
-);
+const Search = () => {
+  const location = useLocation().pathname;
+  return (
+    <div className={location === '/' ? style.container__search : style.container__explore}>
+      <SearchDataSettings />
+      <SearchZoneSettings />
+    </div>
+  );
+};
 
 export default Search;

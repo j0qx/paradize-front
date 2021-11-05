af348f2503ede8a146f5c65daffcd335d9bc10c3
doudou3180 <ferrandpierremarie@gmail.com>
2021-11-05 18:10:57 +0100

    page card offer done ready to fill with data

--- packages/front-react/src/components/CardOffer/CardOffer.js
+++ packages/front-react/src/components/CardOffer/CardOffer.js
@@ -1,19 +1,48 @@
 /* eslint-disable no-trailing-spaces */
+import ImageGallery from 'react-image-gallery';
 import style from './CardOffer.module.scss';
 // import files
-import house from '../../assets/image/house.jpg';
+
 // import components
 import BoardDatas from '../../containers/BoardDatas';
+import 'react-image-gallery/styles/scss/image-gallery.scss';
+import 'react-image-gallery/styles/css/image-gallery.css';
+import image from '../../assets/image/house.jpg';
+import image2 from '../../assets/image/house.jpg';
+import image3 from '../../assets/image/house.jpg';
+import image4 from '../../assets/image/house.jpg';
+import image5 from '../../assets/image/house.jpg';
 
 const CardOffer = () => {
-  const toto = 'tata';
+  const images = [
+    {
+      original: image,
+      thumbnail: image, 
+    },
+    {
+      original: image2,
+      thumbnail: image2, 
+    },
+    {
+      original: image3,
+      thumbnail: image, 
+    }, 
+    {
+      original: image4,
+      thumbnail: image4, 
+    }, 
+    {
+      original: image5,
+      thumbnail: image, 
+    }, 
+  ];
 
   return (
     <div className={style.cardOffer}>
       <div className={style.cardOffer__containerLeft}>
         <div className={style.cardOffer__containerLeft__content}>
           <div className={style.cardOffer__containerLeft__content__mainPicture}>
-            <img className={style.cardOffer__containerLeft__content__mainPicture__picture} src={house} alt="Principale" />
+            <img className={style.cardOffer__containerLeft__content__mainPicture__picture} src={image} alt="Principale" />
           </div>
           <div className={style.cardOffer__containerLeft__content__description}>
             <h2>Titre de l'annonce</h2>
@@ -30,19 +59,14 @@ const CardOffer = () => {
           </div>
         </div>
         <div className={style.cardOffer__sub}>
-          <div className={style.cardOffer__sub__content}>
-            <img className={style.cardOffer__sub__content__picture} src={house} alt="Principale" />
-          </div>
-          <div className={style.cardOffer__sub__all__pictures}>
-            <img className={style.cardOffer__sub__all__picture} src={house} alt="Principale" />
-            <img className={style.cardOffer__sub__all__picture} src={house} alt="Principale" />
-            <img className={style.cardOffer__sub__all__picture} src={house} alt="Principale" />
-            <img className={style.cardOffer__sub__all__picture} src={house} alt="Principale" />
-            <img className={style.cardOffer__sub__all__picture} src={house} alt="Principale" />
-            <img className={style.cardOffer__sub__all__picture} src={house} alt="Principale" />
-            <img className={style.cardOffer__sub__all__picture} src={house} alt="Principale" />
-            <img className={style.cardOffer__sub__all__picture} src={house} alt="Principale" />
-          </div>
+          <ImageGallery
+            items={images}
+            originalHeight="200"
+            slideInterval="4000"
+            thumbnailHeight="200"
+            showBullets="true"
+            autoPlay="true"
+          />
         </div>
       </div>
       <div className={style.cardOffer__container__right}>

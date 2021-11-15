67e84bc51f634cb4f5a8819809eee95907d14936
doudou3180 <ferrandpierremarie@gmail.com>
2021-11-15 17:39:36 +0100

    404 ready to push + contact fix css

--- src/containers/URLError/URLError.js
+++ src/containers/URLError/URLError.js
@@ -1,28 +1,21 @@
 import style from './URLError.module.scss';
 
-import island from '../../assets/image/atlas404.png';
+import city from '../../assets/image/city.jpg';
 // import sea from '../../assets/image/decor.png';
 
-const URLError = () => {
-  const $text = ' 404 not found ';
-  const tabs = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17'];
-
-  return (
-    <div className={style.urlError}>
-      <div className={style.urlError__inner}>
-        <img className={style.urlError__cloudpic} src={island} alt="" />
-        <div className={style.urlError__text}>
-          <div className={style.ui}>
-            {
-            tabs.map((tab) => <h4 key={tab} className={style.ui__text}>{$text}</h4>)
-}
-          </div>
+const URLError = () => (
+  <div className={style.urlError}>
+    <img className={style.urlError__image} src={city} alt="" />
+    <div className={style.urlError__inner}>
+      <div className={style.urlError__text}>
+        <div className={style.ui}>
+          <h1>404<br />NOT FOUND</h1>
         </div>
-        <h3 className={style.urlError__title}>
-          Le bonheur n'est jamais loin, juste pas ici !
-        </h3>
       </div>
+      <h3 className={style.urlError__title}>
+        Le bonheur n'est jamais loin, juste pas ici !
+      </h3>
     </div>
-  );
-};
+  </div>
+);
 export default URLError;

1ef97871cbbf85f0cc7b278fd2a5de30c4121995
doudou3180 <ferrandpierremarie@gmail.com>
2021-11-16 11:17:10 +0100

    add 2 in progress page text

--- src/containers/ListCardFavoriteOffers/ListCardFavoriteOffers.js
+++ src/containers/ListCardFavoriteOffers/ListCardFavoriteOffers.js
@@ -1,8 +1,10 @@
-import './ListCardFavoriteOffers.module.scss';
+import style from './ListCardFavoriteOffers.module.scss';
 
 const ListCardFavoriteOffers = () => (
-  <div>
-    ListCardFavoriteOffers
+  <div className={style.ListCardFavoriteOffers__container}>
+    <h3 className={style.ListCardFavoriteOffers__container__text}>
+      Stay tune , cette fonctionnalité sera bientot disponible
+    </h3>
   </div>
 );
 

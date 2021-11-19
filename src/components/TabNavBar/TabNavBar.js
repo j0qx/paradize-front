dbcb6d78ba78682a57249e47207543d40517689c
Erwin-Bottius <erwinbottius@gmail.com>
2021-11-19 12:09:12 +0100

    updtate close slide from account page

--- src/components/TabNavBar/TabNavBar.js
+++ src/components/TabNavBar/TabNavBar.js
@@ -1,20 +1,39 @@
 import { NavLink, Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
+import { useDispatch, useSelector } from 'react-redux';
 import style from './TabNavBar.module.scss';
+import { TOGGLE_OPEN_SLIDE } from '../../store/actions';
 import BackButton from '../BackButton';
 
 // Check Navlink et creation des routes sans id.
 
-const TabNavBar = ({ navPages }) => (
-
-  <nav className={style.TabNavBar__menu}>
-    <Link to="/explore">
-      <BackButton
-        value="Direction la carte"
-        classCss="button__back"
-      />
-    </Link>
-    {
+const TabNavBar = ({ navPages }) => {
+  const dispatch = useDispatch();
+  const isRightSlideOpen = useSelector((state) => state.domSettings.isRightSlideOpen);
+  const isLeftSlideOpen = useSelector((state) => state.domSettings.isLeftSlideOpen);
+  return (
+    <nav className={style.TabNavBar__menu}>
+      <Link to="/explore">
+        <BackButton
+          value="Direction la carte"
+          classCss="button__back"
+          handleButtonClick={() => {
+            if (isRightSlideOpen === false) {
+              dispatch({
+                type: TOGGLE_OPEN_SLIDE,
+                slide: 'isRightSlideOpen',
+              });
+            }
+            if (isLeftSlideOpen === false) {
+              dispatch({
+                type: TOGGLE_OPEN_SLIDE,
+                slide: 'isLeftSlideOpen',
+              });
+            }
+          }}
+        />
+      </Link>
+      {
       navPages.map(({ linkTo, title }) => (
         <NavLink
           key={linkTo}
@@ -26,8 +45,9 @@ const TabNavBar = ({ navPages }) => (
         </NavLink>
       ))
     }
-  </nav>
-);
+    </nav>
+  );
+};
 
 TabNavBar.propTypes = {
   navPages: PropTypes.arrayOf(
@@ -37,5 +57,4 @@ TabNavBar.propTypes = {
     }).isRequired,
   ).isRequired,
 };
-
 export default TabNavBar;

22ff4ce2b3a8f0793ef93cb82e8dd4f764d005ec
doudou3180 <ferrandpierremarie@gmail.com>
2021-11-19 12:47:06 +0100

    add scroll top ot window +css offer center

--- src/containers/Offer/Offer.js
+++ src/containers/Offer/Offer.js
@@ -1,6 +1,7 @@
 /* eslint-disable max-len */
 import { useParams, Link } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
+import { useEffect } from 'react';
 import ImageGallery from 'react-image-gallery';
 import style from './Offer.module.scss';
 // import files
@@ -13,6 +14,9 @@ import BackButton from '../../components/BackButton';
 import { TOGGLE_OPEN_SLIDE } from '../../store/actions';
 
 const Offer = () => {
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
   const dispatch = useDispatch();
   const isMobile = window.screen.width < 500;
   const { id } = useParams();
@@ -22,9 +26,11 @@ const Offer = () => {
   const isLeftSlideOpen = useSelector((state) => state.domSettings.isLeftSlideOpen);
   // find pour recuperer l'id du user
   const offer = CardOffers.find((element) => element.id === id);
+
   offer.pictures = offer.picture.map((image) => ({
     original: `https://oparadise-back.herokuapp.com/${image}`,
     thumbnail: `https://oparadise-back.herokuapp.com/${image}`,
+
     originalHeight: 350,
   }));
 
@@ -89,10 +95,12 @@ const Offer = () => {
           <ImageGallery
             items={offer.pictures}
             originalWidth="350"
+            originalHeight="400"
             slideInterval="5000"
-            thumbnailHeight="100"
+            thumbnailHeight="200"
             showBullets="true"
             autoPlay="true"
+
           />
 
         </div>
